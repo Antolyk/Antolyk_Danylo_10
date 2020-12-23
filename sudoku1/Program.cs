f2e847cfa3b538795aecf2d9c72dfc6ed9d13200
Antolyk <73401244+Antolyk@users.noreply.github.com>
2020-12-23 08:10:20 +0200

    Sudoku generator
    Sudoku generator

---
+++ sudoku1/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace sudoku1
+{
+    class SudokuGame
+    {
+        static int[,] grid = new int[9, 9];
+        static bool[,] zeroGrid = new bool[9, 9];
+        static string s;
+        static Random ran = new Random();
+        static void Generator()
+        {
+            for(int i = 0; i < grid.GetLength(0); i++)
+            {
+                for(int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = ((i * 3 + i / 3 + j) % 9 + 1);  //Базовое заполнение
+                }
+            }
+        }
+        static void Transponing()                          //Транспонирование
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int tmp = grid[i, j];
+                    grid[i, j] = grid[j, i];
+                    grid[j, i] = tmp;
+                }
+            }
+        }
+
+        static void SwapRows()         //Замена рядов
+        {
+            Random ran1 = new Random();
+            Random ran2 = new Random();
+            Random ran3 = new Random();
+            Random ran4 = new Random();
+            Random ran5 = new Random();
+            Random ran6 = new Random();
+            int first = ran1.Next(1, 3);
+            int second = ran2.Next(1, 3);
+            int third = ran3.Next(4, 6);
+            int fourth = ran4.Next(4, 6);
+            int fifth = ran5.Next(7, 9);
+            int sixth = ran6.Next(7, 9);
+            for (int k = 0; k < 9; k++)
+                {
+                    int tmp = grid[first - 1, k];
+                    grid[first - 1, k] = grid[second - 1, k];
+                    grid[second - 1, k] = tmp;
+                }
+            for (int k = 0; k < 9; k++)
+            {
+                int tmp = grid[third - 1, k];
+                grid[third - 1, k] = grid[fourth - 1, k];
+                grid[fourth - 1, k] = tmp;
+            }
+            for (int k = 0; k < 9; k++)
+            {
+                int tmp = grid[fifth - 1, k];
+                grid[fifth - 1, k] = grid[sixth - 1, k];
+                grid[sixth - 1, k] = tmp;
+            }
+
+        }
+        static void SwapColumns() //Замена столбцов
+        {
+            Transponing();
+            SwapRows();
+            Transponing();           
+        }
+        static void Delete (int x)           //Удаление задач
+        {
+            switch (x)
+            {
+                case 1:
+                    int colToVanish1 = ran.Next(46, 51);
+                    for(int k = 0; k < colToVanish1; k++)
+                    {
+                        int i = ran.Next() % 9;
+                        int j = ran.Next() % 9;
+                        if (!zeroGrid[i,j])
+                        {
+                            grid[i, j] = -1;
+                            zeroGrid[i, j] = !zeroGrid[i,j];
+                        }
+                        else {k--;}                      
+                    }
+                    Console.WriteLine("Easy");
+                    break;
+                case 2:
+                    int colToVanish2 = ran.Next(51, 56);
+                    for (int k = 0; k < colToVanish2; k++)
+                    {
+                        int i = ran.Next() % 9;
+                        int j = ran.Next() % 9;
+                        if (!zeroGrid[i, j])       //Проверка на то, убирали уже эту цифру или нет
+                        {
+                            grid[i, j] = -1;
+                            zeroGrid[i, j] = !zeroGrid[i, j];
+                        }
+                        else { k--; }                      
+                    }
+                    Console.WriteLine("Medium");
+                    break;
+                case 3:
+                    int colToVanish3 = ran.Next(56,61);
+                    for (int k = 0; k < colToVanish3; k++)
+                    {
+                        int i = ran.Next() % 9;
+                        int j = ran.Next() % 9;
+                        if (!zeroGrid[i, j])
+                        {
+                            grid[i, j] = -1;
+                            zeroGrid[i, j] = !zeroGrid[i, j];
+                        }
+                        else { k--; }                    
+                    }
+                    Console.WriteLine("Hard");
+                    break;
+            }
+                
+        }
+
+        static void Draw(ref int[,] grid )    //Отрисовка
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (grid[x, y] == -1) { s += ("  "); }
+                    else { s += (grid[x, y] + " "); }
+                    if (y == 2 || y == 5) { s += ("| "); }        
+                }
+                s += ('\n');
+                if (x == 2 || x == 5) { s += ("------+-------+------\n"); }
+            }
+            Console.WriteLine(s);
+                      
+        }
+           
+        static void Main(string[] args)
+        {
+            s = "";
+            string s2;          
+            Generator();
+            for (int i = 0; i < 1000; i++)
+            {
+                Transponing();
+                SwapRows();
+                SwapColumns();
+            }
+            Console.Write("Выберите сложность(1-3): ");
+           try
+            {
+                int diff = Int32.Parse(Console.ReadLine());               
+                Delete(diff);
+            }
+           catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Draw(ref grid);
+            Console.ReadKey();
+        }
+    }
+}
